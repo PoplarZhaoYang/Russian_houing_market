d47b831cf5c1ea732b5d0adf8217222798acb043
jibancanyang <1599664856@qq.com>
2017-06-04 12:05:22 +0800

    complete first submit, get rank 2223

--- Russian_houing_market/train.py
+++ Russian_houing_market/train.py
@@ -4,7 +4,9 @@
 import xgboost as xgb
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import cross_val_score
 from sklearn import model_selection, preprocessing
+from sklearn import metrics
 
 train_df = pd.read_csv('./data/train.csv', parse_dates=['timestamp'])
 test_df = pd.read_csv('./data/test.csv', parse_dates=['timestamp'])
@@ -24,6 +26,7 @@ train_y = train_df.price_doc.values
 #test_y = test_df.price_doc.values
 
 train_X = train_df.drop(["id", "timestamp", "price_doc"], axis=1)
+test_id = test_df["id"]
 test_X = test_df.drop(["id", "timestamp"], axis=1)
 
 xgb_params = {
@@ -41,8 +44,13 @@ dtest = xgb.DMatrix(test_X, feature_names=test_X.columns.values)
 
 model = xgb.train(xgb_params, dtrain, num_boost_round=100)
 ypred = model.predict(dtest)
+
+
 with open('predict.txt', 'w') as f:
-    for itme in ypred:
-        f.write("%s\n" % (itme))
+    f.write('id,price_doc\n')
+    for ids, itme in zip(test_id, ypred):
+        if itme < 0:
+            itme = -itme
+        f.write("%d,%.2f\n" % (ids, itme))
 
 
